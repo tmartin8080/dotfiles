7d693224f1a9ec777196b9bf3cb1e91b2068dbce
tmartin8080 <troy@devato.com>
2021-02-26 11:41:09 -0500

    Update brew shell

--- brew.sh
+++ brew.sh
@@ -20,28 +20,24 @@ ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
 brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
-# Install a modern version of Bash.
+
 brew install bash
 brew install bash-completion2
 
-# Switch to using brew-installed bash as default shell
-if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
-  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
-  chsh -s "${BREW_PREFIX}/bin/bash";
-fi;
-
-
+brew install zsh
 brew install z
 
-# Install `wget` with IRI support.
-brew install wget --with-iri
+chsh -s "/bin/zsh";
 
-# Install GnuPG to enable PGP-signing commits.
+## Install `wget` with IRI support.
+brew install wget 
+brew install curl 
+brew install git 
+
+## Install GnuPG to enable PGP-signing commits.
 brew install gnupg
 
-# Install more recent versions of some macOS tools.
+## Install more recent versions of some macOS tools.
 brew install --HEAD neovim
 brew install grep
 brew install openssh
@@ -49,56 +45,11 @@ brew install screen
 brew install gmp
 brew install rg
 
-# Install font tools.
+## Install font tools.
 brew tap bramstein/webfonttools
 brew install sfnt2woff
 brew install sfnt2woff-zopfli
 brew install woff2
 
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
-
-# Install other useful binaries.
-brew install ack
-#brew install exiv2
-brew install git
-brew install git-lfs
-brew install gs
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rlwrap
-brew install ssh-copy-id
-brew install tree
-brew install vbindiff
-brew install zopfli
-
-# Remove outdated versions from the cellar.
+## Remove outdated versions from the cellar.
 brew cleanup
